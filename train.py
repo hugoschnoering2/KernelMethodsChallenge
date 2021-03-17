463e59df62fe892cac2ca268b230f316f9ed3f53
Julien Duquesne <ju.duquesne@hotmail.fr>
2021-03-17 15:36:18 +0100

    add string data

--- train.py
+++ train.py
@@ -9,9 +9,14 @@ from estimators.logistic_ridge_estimator import KernelLogRidgeEstimator
 
 
 def train(args):
-    Xtr0, Xtr1, Xtr2 = pd.read_csv("data/Xtr0_mat100.csv", header=None), \
-                       pd.read_csv("data/Xtr1_mat100.csv", header=None), \
-                       pd.read_csv("data/Xtr2_mat100.csv", header=None)
+    if args.data_type == "float":
+        Xtr0, Xtr1, Xtr2 = pd.read_csv("data/Xtr0_mat100.csv", header=None, delimiter=" ").astype(float), \
+                           pd.read_csv("data/Xtr1_mat100.csv", header=None, delimiter=" ").astype(float), \
+                           pd.read_csv("data/Xtr2_mat100.csv", header=None, delimiter=" ").astype(float)
+    elif args.data_type == "string":
+        Xtr0, Xtr1, Xtr2 = pd.read_csv("data/Xtr0.csv", index_col="Id"), \
+                           pd.read_csv("data/Xtr1.csv", index_col="Id"), \
+                           pd.read_csv("data/Xtr2.csv", index_col="Id")
     X = pd.concat([Xtr0, Xtr1, Xtr2], axis=0).values
     Ytr0, Ytr1, Ytr2 = pd.read_csv("data/Ytr0.csv", index_col="Id"), \
                        pd.read_csv("data/Ytr1.csv", index_col="Id"), \
@@ -24,9 +29,12 @@ def train(args):
         print(f"Fold {i + 1}")
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index].reshape(-1), y[test_index].reshape(-1)
-        model = KernelLogRidgeEstimator(args.kernel, args.alpha)
+        if args.model == "logistic_regression":
+            model = KernelLogRidgeEstimator(args.kernel, args.alpha)
         model.fit(X_train, y_train)
         preds = model.predict(X_test)
+        print(preds)
+        print(y_test)
         score = np.sum(preds == y_test) / len(preds)
         print(f"Val accuracy: {score}")
         scores.append(score)
@@ -35,8 +43,10 @@ def train(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", type=str, default="logistic_regression", choices=["logistic_regression"])
     parser.add_argument("-k", "--kernel", type=str, default="spectrum")
     parser.add_argument("-f", "--folds", type=int, default=4)
     parser.add_argument("-a", "--alpha", type=float, default=0.1)
+    parser.add_argument("-t", "--data_type", type=str, choices=['string', 'float'], default="string")
     args = parser.parse_args()
     train(args)
