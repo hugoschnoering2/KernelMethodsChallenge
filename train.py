8ebd05ce94f7272412b083b0b74c84a6f9cccedd
Julien Duquesne <ju.duquesne@hotmail.fr>
2021-03-17 16:35:11 +0100

    fix dataset concat

--- train.py
+++ train.py
@@ -7,23 +7,10 @@ from sklearn.model_selection import KFold
 from estimators.logistic_ridge_estimator import KernelLogRidgeEstimator
 
 
-def train(args):
-    if args.data_type == "float":
-        Xtr0, Xtr1, Xtr2 = pd.read_csv("data/Xtr0_mat100.csv", header=None, delimiter=" ").astype(float), \
-                           pd.read_csv("data/Xtr1_mat100.csv", header=None, delimiter=" ").astype(float), \
-                           pd.read_csv("data/Xtr2_mat100.csv", header=None, delimiter=" ").astype(float)
-    elif args.data_type == "string":
-        Xtr0, Xtr1, Xtr2 = pd.read_csv("data/Xtr0.csv", index_col="Id"), \
-                           pd.read_csv("data/Xtr1.csv", index_col="Id"), \
-                           pd.read_csv("data/Xtr2.csv", index_col="Id")
-    X = pd.concat([Xtr0, Xtr1, Xtr2], axis=0).values
-    Ytr0, Ytr1, Ytr2 = pd.read_csv("data/Ytr0.csv", index_col="Id"), \
-                       pd.read_csv("data/Ytr1.csv", index_col="Id"), \
-                       pd.read_csv("data/Ytr2.csv", index_col="Id")
-    y = pd.concat([Ytr0, Ytr1, Ytr2], ignore_index=True, axis=0).values
-
+def cross_validate(args, X, y):
     kf = KFold(args.folds, random_state=0, shuffle=True)
     scores = []
+
     for i, (train_index, test_index) in enumerate(kf.split(X)):
         print(f"Fold {i + 1}")
         X_train, X_test = X[train_index], X[test_index]
@@ -32,12 +19,31 @@ def train(args):
             model = KernelLogRidgeEstimator(args.kernel, args.alpha)
         model.fit(X_train, y_train)
         preds = model.predict(X_test)
-        print(preds)
-        print(y_test)
         score = np.sum(preds == y_test) / len(preds)
         print(f"Val accuracy: {score}")
         scores.append(score)
     print(f"Average accuracy over folds: {np.mean(scores)}")
+
+
+def train(args):
+    if args.data_type == "float":
+        Xtr0, Xtr1, Xtr2 = pd.read_csv("data/Xtr0_mat100.csv", header=None, delimiter=" ").astype(float).values, \
+                           pd.read_csv("data/Xtr1_mat100.csv", header=None, delimiter=" ").astype(float).values, \
+                           pd.read_csv("data/Xtr2_mat100.csv", header=None, delimiter=" ").astype(float).values
+    elif args.data_type == "string":
+        Xtr0, Xtr1, Xtr2 = pd.read_csv("data/Xtr0.csv", index_col="Id").values, \
+                           pd.read_csv("data/Xtr1.csv", index_col="Id").values, \
+                           pd.read_csv("data/Xtr2.csv", index_col="Id").values
+    Ytr0, Ytr1, Ytr2 = pd.read_csv("data/Ytr0.csv", index_col="Id").values, \
+                       pd.read_csv("data/Ytr1.csv", index_col="Id").values, \
+                       pd.read_csv("data/Ytr2.csv", index_col="Id").values
+
+    print("## DATASET 1 ##")
+    cross_validate(args, Xtr0, Ytr0)
+    print("## DATASET 2 ##")
+    cross_validate(args, Xtr1, Ytr1)
+    print("## DATASET 3 ##")
+    cross_validate(args, Xtr2, Ytr2)
 
 
 if __name__ == '__main__':
