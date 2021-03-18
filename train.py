f261ad28eb3d71f49a775e13196f133300d3b729
Julien Duquesne <ju.duquesne@hotmail.fr>
2021-03-18 21:53:20 +0100

    restore train.py

--- train.py
+++ train.py
@@ -4,7 +4,9 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold
 
-
+from estimators.ridge_classifier_estimator import RidgeClassifier
+from estimators.ridge_regression_estimator import RidgeRegression
+from estimators.svm_estimator import SVM
 
 
 def cross_validate(args, X, y):
@@ -15,15 +17,19 @@ def cross_validate(args, X, y):
         print(f"Fold {i + 1}")
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index].reshape(-1), y[test_index].reshape(-1)
-        if args.model == "logistic_regression":
-            from estimators.ridge_classifier_estimator import ridge_classifier
-            model = ridge_classifier(args.kernel, args.alpha)
+        if args.model == "svm":
+            model = SVM(kernel=args.kernel, alpha=args.alpha, m=args.m, k=args.k)
+        elif args.model == "ridge_classifier":
+            model = RidgeClassifier(kernel=args.kernel, alpha=args.alpha, m=args.m, k=args.k)
+        elif args.model == "ridge_regression":
+            model = RidgeRegression(kernel=args.kernel, alpha=args.alpha, m=args.m, k=args.k)
+
         model.fit(X_train, y_train)
-        preds = model.predict(X_test)
-        score = np.sum(preds == y_test) / len(preds)
+        score = model.score(X_test, y_test)
         print(f"Val accuracy: {score}")
         scores.append(score)
     print(f"Average accuracy over folds: {np.mean(scores)}")
+    return np.mean(scores)
 
 
 def train(args):
@@ -32,25 +38,28 @@ def train(args):
                            pd.read_csv("data/Xtr1_mat100.csv", header=None, delimiter=" ").astype(float).values, \
                            pd.read_csv("data/Xtr2_mat100.csv", header=None, delimiter=" ").astype(float).values
     elif args.data_type == "string":
-        Xtr0, Xtr1, Xtr2 = pd.read_csv("data/Xtr0.csv", index_col="Id").values, \
-                           pd.read_csv("data/Xtr1.csv", index_col="Id").values, \
-                           pd.read_csv("data/Xtr2.csv", index_col="Id").values
-    Ytr0, Ytr1, Ytr2 = pd.read_csv("data/Ytr0.csv", index_col="Id").values, \
-                       pd.read_csv("data/Ytr1.csv", index_col="Id").values, \
-                       pd.read_csv("data/Ytr2.csv", index_col="Id").values
+        Xtr0, Xtr1, Xtr2 = pd.read_csv("data/Xtr0.csv", index_col="Id").values[:, 0], \
+                           pd.read_csv("data/Xtr1.csv", index_col="Id").values[:, 0], \
+                           pd.read_csv("data/Xtr2.csv", index_col="Id").values[:, 0]
+    Ytr0, Ytr1, Ytr2 = pd.read_csv("data/Ytr0.csv", index_col="Id").replace(0, -1).values[:, 0], \
+                       pd.read_csv("data/Ytr1.csv", index_col="Id").replace(0, -1).values[:, 0], \
+                       pd.read_csv("data/Ytr2.csv", index_col="Id").replace(0, -1).values[:, 0]
 
     print("## DATASET 1 ##")
-    cross_validate(args, Xtr0, Ytr0)
+    avg1 = cross_validate(args, Xtr0, Ytr0)
     print("## DATASET 2 ##")
-    cross_validate(args, Xtr1, Ytr1)
+    avg2 = cross_validate(args, Xtr1, Ytr1)
     print("## DATASET 3 ##")
-    cross_validate(args, Xtr2, Ytr2)
+    avg3 = cross_validate(args, Xtr2, Ytr2)
+    print(f"Average score over all dataset:{np.mean([avg1, avg2, avg3])}")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model", type=str, default="logistic_regression", choices=["logistic_regression"])
-    parser.add_argument("-k", "--kernel", type=str, default="spectrum")
+    parser.add_argument("-md", "--model", type=str,default="svm", choices=["svm", "ridge_regression", "ridge_classifier"])
+    parser.add_argument("-m", "--m", type=int, default=0)
+    parser.add_argument("-k", "--k", type=int, default=6)
+    parser.add_argument("-kn", "--kernel", type=str, default="mismatch", choices=["mismatch"])
     parser.add_argument("-f", "--folds", type=int, default=4)
     parser.add_argument("-a", "--alpha", type=float, default=0.1)
     parser.add_argument("-t", "--data_type", type=str, choices=['string', 'float'], default="string")
